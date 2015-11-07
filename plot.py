96e45779cd4c5671d0381584b73f9997578ba677
dragonly <liyilongko@163.com>
2015-11-07 13:27:04 +0800

    make plot.py a command line tool

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
+import argparse
 from settings import H, E, M, wordsOfEachTopic as wot, T
 
 from settings import outputDir
@@ -19,7 +20,15 @@ def showTopic(jsonFile):
     x = np.arange(T)
     for e in range(data['E']):
         for h in range(data['H']):
+            print len(x), np.shape(n_het), np.shape(axs)
             sns.barplot(x, n_het[h,e,:], palette="Set3", ax=axs[e][h])
             axs[e][h].set_ylabel("counts")
             axs[e][h].set_title("h:%d e:%d" % (h,e))
     plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('json_file', type=str, help='input file in json format')
+    args = parser.parse_args()
+
+    showTopic(args.json_file)
