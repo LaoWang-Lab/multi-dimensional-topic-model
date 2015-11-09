54b8cf174d5ab28e964b3eaaa83c0c471bea7428
dragonly <liyilongko@163.com>
2015-11-10 00:42:54 +0800

    na

--- dance.py
+++ dance.py
@@ -6,6 +6,7 @@ import os
 from plot import plot_fig_in_dir
 
 def dance(png_dir, gif):
+    os.makedirs(os.path.pathname(git))
     os.chdir(png_dir)
     if gif in os.listdir('.'):
         os.remove(gif)
@@ -20,9 +21,13 @@ def dance_all():
         dirs.append(hewotm)
         for run in os.listdir(os.path.sep.join(dirs)):
             dirs.append(run)
-            print(os.path.sep.join(dirs))
-            plot_fig_in_dir(os.path.sep.join(dirs))
-            dance(os.path.sep.join(dirs), os.path.sep.join(dirs[0] + ['gif'] + '_'.join(dirs[2:])))
+            dir_name = os.path.sep.join(dirs)
+            print(dir_name)
+            if 'done' not in os.listdir(dir_name):
+                plot_fig_in_dir(dir_name)
+                dance(dir_name, os.path.sep.join(dirs[0] + ['gif'] + '_'.join(dirs[2:])))
+                with open('done','w') as fd:
+                    pass
             dirs.pop()
         dirs.pop()
 
