e5fc6525e3572537ccebddfec3f9c539de0f84a4
yuealves <yuealves@163.com>
2015-11-24 01:57:13 +0800

    add another dataset

--- cymlda.py
+++ cymlda.py
@@ -23,6 +23,8 @@ class mylda:
 
         self._dictionary = {x[:-1]:i for (i, x) in enumerate(open(dictionary))}
         self.T = len(self._dictionary)
+        self.H = H
+        self.E = E
 
         if docset == 'bagOfWords':
             self._docset = docset
@@ -33,10 +35,11 @@ class mylda:
             self._docDir = docDir
             self.M = len(os.listdir(docDir))
 
-        self._n_mh = np.zeros((self.M, H)) # counts for words in document m which were labeled as in dimension h
-        self._n_het = np.zeros((H, E, self.T)) # counts for word type t in topic h,e
-        self._n_he = np.zeros((H, E)) # counts for documents which were labeled as in topic e for dimension h
+        self._n_mh = np.zeros((self.M, H), dtype=np.int32) # counts for words in document m which were labeled as in dimension h
+        self._n_het = np.zeros((H, E, self.T), dtype=np.int32) # counts for word type t in topic h,e
+        self._n_he = np.zeros((H, E), dtype=np.int32) # counts for documents which were labeled as in topic e for dimension h
         self._z_mh = np.random.randint(E, size=(self.M, H)) # value of zi for document m and dimension h
+        self._z_mh = np.asarray(self._z_mh, dtype=np.int32)
         self._w_mi = [[],]* self.M # value of wi for document m
         self._s_mi = [[],] * self.M # value of si for document m
         self.n_loaded = 0 # counts for documents loaded
@@ -55,7 +58,6 @@ class mylda:
         
         n = len(self._w_mi[m])
         self._n_mh[m] = np.random.multinomial(n, (1/H,)*H)
-        self._z_mh[m] = np.random.randint(E, size=H)
         self._s_mi[m] = np.random.permutation(n2s(self._n_mh[m]))
 
         for (s, z) in enumerate(self._z_mh[m]):
@@ -94,42 +96,51 @@ class mylda:
         if self._docset == 'bagOfWords':
 
             printFlag = True
+            corpusFile = open(self._docDir).readlines()
+            self.N_total_words = int(corpusFile[2])
+            self._w_mi_ = np.zeros(int(self.N_total_words*1.5), dtype=np.int32)
+            self._n_mw = np.zeros((2, self.M), dtype=np.int32)
+            self.cum = 0
             for record in open(self._docDir).readlines()[3:]:
                 m, w, counts = [int(x) for x in record.split()]
                 m -= 1 # 1 in corpus file is corresponding to 0 in our model
                 w -= 1
+                self._w_mi_[self.cum:self.cum+counts] = w
+                self._n_mw[0,m] += counts
+                self.cum += counts
+                if m < self.M - 1:
+                    self._n_mw[1,m+1] = self.cum
                 if m%20 == 0 and printFlag:
                     print('read doc %d' % m)
                     printFlag = False
                 elif m%20 != 0:
                     printFlag = True
-                self._w_mi[m].extend([w] * counts)
+                # self._w_mi[m].extend([w] * counts)
                 # self._w_mi_tmp[m] += [w] * counts
+            print('cum is %d' % self.cum)
+            self._w_mi_ = self._w_mi_[:self.cum]
+            self._s_mi_ = np.random.randint(self.H, size=self.cum)
+            self._s_mi_ = np.asarray(self._s_mi_, dtype=np.int32)
 
             self.n_loaded = m
 
             for m in range(self.M):
                 if m%20 == 0:
                     print('initialize doc %d' % m)
-                self._w_mi[m] = np.random.permutation(np.array(self._w_mi[m]))
-                n = len(self._w_mi[m])
-                self._n_xmh[m] = np.random.multinomial(n, (1/H,)*H)
-                self._z_mh[m] = np.random.randint(E, size=H)
-                self._s_mi[m] = np.random.permutation(n2s(self._n_mh[m]))
-
+                s_counts = np.bincount(self._s_mi_[self._n_mw[1,m]:self._n_mw[1,m]+self._n_mw[0,m]])
+                self._n_mh[m,0:len(s_counts)] = s_counts
                 for (s, z) in enumerate(self._z_mh[m]):
                     self._n_he[s][z] += 1
 
-                for (i, s) in enumerate(self._s_mi[m]):
-                    self._n_het[s, self._z_mh[m,s], self._w_mi[m][i]] += 1
+                for i,s in enumerate(self._s_mi_[self._n_mw[1,m]:self._n_mw[1,m]+self._n_mw[0,m]]):
+                    self._n_het[s, self._z_mh[m,s], self._w_mi_[i]] += 1
 
         else:
             for i, docName in enumerate(os.listdir(self._docDir)):
                 if i % 20 == 0:
                     print('read docs: %d' % i)
                 self.readDoc(os.path.join(self._docDir, docName))
-
-        self.list2np()
+                self.list2np()
 
     def test_n(self):
         for m in range(self.M):
